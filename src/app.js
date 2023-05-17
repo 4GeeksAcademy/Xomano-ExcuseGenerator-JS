f20fd739e9b91ac44f070c1f150375269739aadc
Xomano <129993903+Xomano@users.noreply.github.com>
2023-05-17 20:26:17 +0100

    creating the full code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+	let who = ['The dog','My grandma','His turtle','My bird'];
+	let action = ['ate','peed on','crushed','broke'];
+	let what = ['my homework', 'the keys', 'the car', 'the money'];
+	let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+	
+	
+	function getRandom(range) {
+		return Math.floor(Math.random() * range);
+	};
+	
+	function createExcuse(){
+		let excuse =
+		who[getRandom(4)] +
+		" " +
+		action[getRandom(4)] +
+		" " +
+		what[getRandom(4)] +
+		" " +
+		when[getRandom(5)];
+
+		document.getElementById("excuse").innerHTML = excuse;
+	};

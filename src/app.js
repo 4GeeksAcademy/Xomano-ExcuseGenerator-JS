cfaf4a3fdc743ce02bd2aa22cd8db4a259b8545c
Xomano <129993903+Xomano@users.noreply.github.com>
2023-05-28 16:38:21 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
+function getRandom(range) {
+	return Math.floor(Math.random() * range);
+};
 
-
-
-	let who = ['The dog','My grandma','His turtle','My bird'];
-	let action = ['ate','peed on','crushed','broke'];
-	let what = ['my homework', 'the keys', 'the car', 'the money'];
-	let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+const who = ['The dog','My grandma','His turtle','My bird'];
+const action = ['ate','peed on','crushed','broke'];
+const what = ['my homework', 'the keys', 'the car', 'the money'];
+const when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 	
 	
-	function getRandom(range) {
-		return Math.floor(Math.random() * range);
-	};
 	
-	function createExcuse(){
-		let excuse =
-		who[getRandom(4)] +
-		" " +
-		action[getRandom(4)] +
-		" " +
-		what[getRandom(4)] +
-		" " +
-		when[getRandom(5)];
-
-		document.getElementById("excuse").innerHTML = excuse;
-	};
+function createExcuse(){
+	let excuse =
+	who[getRandom(who.length)] +
+	" " +
+	action[getRandom(action.length)] +
+	" " +
+	what[getRandom(what.length)] +
+	" " +
+	when[getRandom(when.length)];
+	document.getElementById("excuse").innerHTML = excuse;
+};
